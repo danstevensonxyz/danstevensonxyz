39a55c69d13cfba3ade5a76648067dac7f19988b
DanStevensonCO <danstevensonco@gmail.com>
2021-03-07 21:10:35 +0000

    import additional css files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-coy.css"
+import "prismjs/themes/prism-coy.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
@@ -5,9 +11,3 @@ import "typeface-merriweather"
 import "./src/normalize.css"
 // custom CSS styles
 import "./src/style.css"
-
-// Highlighting for code blocks
-// import "prismjs/themes/prism.css"
-import "prismjs/themes/prism-coy.css"
-require("prismjs/themes/prism-coy.css");
-require("prismjs/plugins/line-numbers/prism-line-numbers.css")
