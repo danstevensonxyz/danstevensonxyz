bc16488d36b5ba1c36b6a3462a0f55b15c51065b
DanStevensonCO <danstevensonco@gmail.com>
2021-03-17 16:32:41 +0000

    add recent projects block with three item blocks and images for each

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,60 +5,102 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import DapImg from "./projects/dap-apply-for-free-school-meals.png"
+import VersifyImg from "./projects/versify-logo-green-background.png"
+import Version1Img from "./projects/danstevenson-co-v1.png"
+
+export function DapImgImport() {
+  return <img src={DapImg} class="project-block-image" alt="Image of the Sheffield City Council free school meals application form I built as part of the Digital Acceleration Project"/>
+}
+
+export function VersifyImgImport() {
+  return <img src={VersifyImg} class="project-block-image" alt="Image of the Versify logo with green background"/>
+}
+
+export function Version1ImgImport() {
+  return <img src={Version1Img} class="project-block-image" alt="Image of version 1 of this website with no navbar or colour and three blog posts"/>
+}
+
+
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
-
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+      <div class="projects-grid">
+        <h1>Recent projects</h1>
+
+        <div class="project-block-one-third">
+            {/* <a href=""> */}
+              <Version1ImgImport />
+            {/* </a> */}
+            <div class="project-block-content">
+              {/* <a href=""> */}
+                <h2 class="project-block-title">Blog and portfolio website</h2>
+              {/* </a> */}
+              <p class="project-block-description">Launched at the beginning of my Northcoders journey, this site has evolved since the beginning of 2021 to include blog posts about my experience with Northcoders and past projects. Built using Gatsby, version-controlled with GitHub and hosted with Netlify, it's a project to give me a place to talk about my projects!</p>
+          </div>
+        </div>
+
+        <div class="project-block-one-third">
+          {/* <a href=""> */}
+            <DapImgImport />
+          {/* </a> */}
+          <div class="project-block-content">
+            {/* <a href=""> */}
+              <h2 class="project-block-title">"Digital Acceleration" forms project</h2>
+            {/* </a> */}
+            <p class="project-block-description">I built and launched over 100 forms in 12 months on the Sheffield City Council website, using the AEM content management system and JavaScript for routing and show/hide logic. This included forms for free school meals, disabled persons' travel passes and complex coronavirus forms.</p>
+          </div>
+        </div>
+
+        <div class="project-block-one-third">
+          {/* <a href=""> */}
+            <VersifyImgImport />
+          {/* </a> */}
+          <div class="project-block-content">
+            {/* <a href=""> */}
+              <h2 class="project-block-title">Versify online language exchange</h2>
+            {/* </a> */}
+            <p class="project-block-description">While at university I founded the Language Exchange Society, which later became Versify, the online Language Exchange. Working with two other co-founders, we built the web app which allowed language students to practice through face-to-face video chats (before anyone had heard of Zoom!)</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="latest-posts">
+      <h1>Latest posts</h1>
+        <ol class="latest-posts-list" style={{ listStyle: `none` }}>
+          {posts.map(post => {
+            const title = post.frontmatter.title || post.fields.slug
+
+            return (
+              <li key={post.fields.slug} className="post-list-item">
+                  <header>
+                    <h2>
+                      <Link to={post.fields.slug} itemProp="url">
+                        {title}
+                      </Link>
+                    </h2>
+                    <p>{post.frontmatter.date}</p>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: post.frontmatter.description || post.excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                  </section>
+              </li>
+            )
+          })}
+        </ol>
+      </div>
     </Layout>
   )
 }
