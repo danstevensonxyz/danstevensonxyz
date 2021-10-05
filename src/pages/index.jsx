7699086481f468eced8f956cb0ca3921a8199573
DanStevensonCO <danstevensonco@gmail.com>
2021-10-05 10:57:19 +0100

    add bio text and blog post feed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,72 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const BlogIndex = ({ location }) => {
+const Homepage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes
+
   return (
-    <Layout location={location}>
-      <SEO title="Home" />
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Latest posts" />
+      
+      <div className="bio-intro">
+        <h1>I'm a software developer, entrepreneur and adventurer.</h1>
+      </div>
+      
+      <div class="latest-posts">
+      <h2>Latest posts</h2>
+        <ol class="latest-posts-list" style={{ listStyle: `none` }}>
+          {posts.map(post => {
+              const title = post.frontmatter.title || post.fields.slug
+
+              return (
+                <li key={post.fields.slug} className="post-list-item">
+                    <header>
+                      <h2>
+                        <Link to={post.fields.slug} itemProp="url">
+                          {title}
+                        </Link>
+                      </h2>
+                      <p>{post.frontmatter.date}</p>
+                    </header>
+                    <section>
+                      <p
+                        dangerouslySetInnerHTML={{
+                          __html: post.frontmatter.description || post.excerpt,
+                        }}
+                        itemProp="description"
+                      />
+                    </section>
+                </li>
+              )
+          })}
+        </ol>
+      </div>
     </Layout>
   )
 }
 
-export default BlogIndex
+export default Homepage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "dddd, DD MMMM yyyy")
+          title
+          description
+        }
+      }
+    }
+  }
+`
