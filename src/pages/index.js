d7ba03b367ce008c9b465af453b72535a5bdbcc4
DanStevensonCO <danstevensonco@gmail.com>
2021-10-05 09:50:48 +0100

    remove blog post list and unnessary metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,70 +4,12 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
-
+const BlogIndex = ({ location }) => {
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="Homepage" />
-
-      <div class="latest-posts">
-      <h1>Latest posts</h1>
-        <ol class="latest-posts-list" style={{ listStyle: `none` }}>
-          {posts.map(post => {
-            if(post.fields.slug.startsWith("/blog")){
-              const title = post.frontmatter.title || post.fields.slug
-
-              return (
-                <li key={post.fields.slug} className="post-list-item">
-                    <header>
-                      <h2>
-                        <Link to={post.fields.slug} itemProp="url">
-                          {title}
-                        </Link>
-                      </h2>
-                      <p>{post.frontmatter.date}</p>
-                    </header>
-                    <section>
-                      <p
-                        dangerouslySetInnerHTML={{
-                          __html: post.frontmatter.description || post.excerpt,
-                        }}
-                        itemProp="description"
-                      />
-                    </section>
-                </li>
-              )
-            }
-          })}
-        </ol>
-      </div>
+    <Layout location={location}>
+      <SEO title="Home" />
     </Layout>
   )
 }
 
 export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "dddd, DD MMMM yyyy")
-          title
-          description
-        }
-      }
-    }
-  }
-`
